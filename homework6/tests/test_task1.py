1c159f4350d809304e2d2ddc0f2703361c3cad48
Anastasiya <nastin.marlis@gmail.com>
2020-11-23 20:06:53 +0300

    fix tests for homework06 task01

--- homework6/tests/test_task1.py
+++ homework6/tests/test_task1.py
@@ -3,22 +3,20 @@ from unittest.mock import patch
 from homework6.task1.counter import instances_counter
 
 
+@instances_counter
 class A:
     pass
 
 
-@patch.object(A, "__init__", lambda self, x: None)
 def test_instance_counter_get():
-    instances_counter(A)
     assert A.get_created_instances() == 0
-    a, _, _ = A(1), A(2), A(3)
+    a, _, _ = A(), A(), A()
     assert a.get_created_instances() == 3
+    a.reset_instances_counter()
 
 
-@patch.object(A, "__init__", lambda self, x: None)
 def test_instance_counter_reset():
-    instances_counter(A)
     assert A.reset_instances_counter() == 0
-    a, _, _ = A(1), A(2), A(3)
+    a, _, _ = A(), A(), A()
     assert a.reset_instances_counter() == 3
     assert a.get_created_instances() == 0
