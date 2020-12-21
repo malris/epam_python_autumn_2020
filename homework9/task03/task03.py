de64ad11bad7fb2ee1b63de45820e75f6ae2f659
Anastasiya <nastin.marlis@gmail.com>
2020-12-21 20:32:06 +0300

    fix homework09 task03

--- homework9/task03/task03.py
+++ homework9/task03/task03.py
@@ -4,32 +4,26 @@ It will count lines in all files with that extension if there are no tokenizer.
 If a the tokenizer is not none, it will count tokens.
 """
 from pathlib import Path
-from typing import Callable, Iterator, Optional, TextIO, Union
+from typing import Callable, Iterable, Optional, TextIO
 
 
-def get_file_instances(path: Path, file_extension: str):
-    pattern = ".".join(["**/*", file_extension])
+def get_file_instances(path: Path, file_extension: str) -> Iterable[TextIO]:
+    pattern = f"**/*.{file_extension}"
     for file in path.glob(pattern):
-        yield open(file)
-
-
-def get_elements_to_count(
-    file: TextIO, tokenizer: Optional[Callable]
-) -> Union[TextIO, Iterator]:
-    return file if tokenizer is None else map(tokenizer, file)
+        with open(file) as f_it:
+            yield f_it
 
 
 def universal_file_counter(
     dir_path: Path, file_extension: str, tokenizer: Optional[Callable] = None
 ) -> int:
     list_file = get_file_instances(dir_path, file_extension)
-
     amount_of_elements = 0
-    for file in list_file:
-        for el in get_elements_to_count(file, tokenizer):
-            amount_of_elements += 1 if tokenizer is None else len(el)
 
     for file in list_file:
-        file.close()
+        elements_to_count = map(tokenizer, file) if tokenizer else file
+        amount_of_elements += sum(
+            len(el) if tokenizer else 1 for el in elements_to_count
+        )
 
     return amount_of_elements
